c99935270b8c3b748c6f16c183c5c03b5ef74c10
star <s0913940588@gmail.com>
2017-11-13 15:16:13 +0800

    RecyclerView

--- app/src/main/java/com/star/demo2017111303/MainActivity.java
+++ app/src/main/java/com/star/demo2017111303/MainActivity.java
@@ -17,24 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        StudentDAOMemoryImpl t = new StudentDAOMemoryImpl();
-        t.add(new Student("Bob", "123", "aabb"));
-        t.add(new Student("Mary", "234", "ccdd"));
 
-        Student[] mylist = t.getData();
-        for (Student s: mylist)
-        {
-            Log.d("DATAS", s.toString());
-        }
 
-        Student editStudent = mylist[0];
-        editStudent.tel = "987";
-        t.updata(editStudent);
 
-        Student[] mylist1 = t.getData();
-        for (Student s: mylist1)
-        {
-            Log.d("DATAS", "update:" + s.toString());
-        }
+//        StudentDAOMemoryImpl t = new StudentDAOMemoryImpl();
+//        t.add(new Student("Bob", "123", "aabb"));
+//        t.add(new Student("Mary", "234", "ccdd"));
+//
+//        Student[] mylist = t.getData();
+//        for (Student s: mylist)
+//        {
+//            Log.d("DATAS", s.toString());
+//        }
+//
+//        Student editStudent = mylist[0];
+//        editStudent.tel = "987";
+//        t.updata(editStudent);
+//
+//        Student[] mylist1 = t.getData();
+//        for (Student s: mylist1)
+//        {
+//            Log.d("DATAS", "update:" + s.toString());
+//        }
     }
 }
