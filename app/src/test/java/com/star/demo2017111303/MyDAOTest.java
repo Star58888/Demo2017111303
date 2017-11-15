04b4c4ab37883c81fa5e2d3b93838b8581c2673c
star <s0913940588@gmail.com>
2017-11-15 09:06:12 +0800

    assertEquals 斷言 測試

--- app/src/test/java/com/star/demo2017111303/MyDAOTest.java
+++ app/src/test/java/com/star/demo2017111303/MyDAOTest.java
@@ -1,5 +1,8 @@
 package com.star.demo2017111303;
 
+import com.star.demo2017111303.Data.Student;
+import com.star.demo2017111303.Data.StudentDAOMemoryImpl;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,8 +13,26 @@ import static org.junit.Assert.assertEquals;
 
 public class MyDAOTest {
     @Test
-    public void MyTest1()
-    {
-        assertEquals(3+3,6);
+    public void MyTest1() {
+        assertEquals(3 + 3, 6);
+    }
+
+    @Test
+    public void TestMemoryDAO1() {
+        StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
+        dao.add(new Student("AA", "11", "aabb"));
+        dao.add(new Student("BB", "22", "aabb"));
+        dao.add(new Student("CC", "33", "aabb"));
+        Student[] stus = dao.getData();
+        assertEquals(stus.length, 3);
+    }
+    @Test
+    public void TestMemoryDAO2() {
+        StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
+        dao.add(new Student("AA", "11", "aabb"));
+        dao.add(new Student("BB", "22", "aabb"));
+        dao.add(new Student("CC", "33", "aabb"));
+        Student[] stus = dao.getData();
+        assertEquals(stus[2].name, "CC");
     }
 }
